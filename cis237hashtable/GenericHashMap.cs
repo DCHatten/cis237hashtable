9b553b7d50e85e87fe58946c9ef49cfa6bb25cd9
David Hatten <dhatten87@gmail.com>
2016-11-03 17:30:23 -0400

    Built a hash function and showed its use, then used the built in dictionary function to complete the same goal but with collision handling

---
+++ cis237hashtable/GenericHashMap.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237hashtable
+{
+    class GenericHashMap<TKey, TValue>
+    {
+        //Declare a constant for the prime value to use in the Hash Function
+        private const int PRIMEVALUE = 97;
+        //Declare an array for the values
+        TValue[] theValues;
+        TKey[] theKeys;
+        //Constructor
+        public GenericHashMap()
+        {
+            theValues = new TValue[PRIMEVALUE];
+            theKeys = new TKey[PRIMEVALUE];
+
+        }
+        //Put function to put a key value pair in to the array/hash table
+        public void Put(TKey key, TValue value)
+        {
+            //Get the index to put the value into
+            int indexForValue = hashFunction(key);
+            //Store the value in the array
+            theValues[indexForValue] = value;
+            //Storing the key in a parallel array for later display
+            theKeys[indexForValue] = key;
+        }
+        //Get a value out using the passed in key.
+        public TValue Get(TKey key)
+        {
+            //Get the index to put the value into
+            int indexForValue = hashFunction(key);
+            //Get the value out of the array with the calculated key
+            return theValues[indexForValue];
+        }
+        //Ye Olde Hash Function
+        private int hashFunction(TKey key)
+        {
+            //Use the built in GetHashCode method that comes from object to 
+            //get an integer value that can be modded with the PRIMEVALUE.
+            //This will give us and integer between 0 and the PRIMEVALUE.
+            return key.GetHashCode() % PRIMEVALUE;
+        }
+        //Method to print the contents of the array
+        public override string ToString()
+        {
+            string returnString = "\t\tKey => Value";
+            returnString += Environment.NewLine + "---------------------------------------------";
+            for (int i = 0; i < theValues.Length; i++)
+            {
+                if (theValues[i] != null)
+                {
+                    returnString += Environment.NewLine + theKeys[i] + " => " + theValues[i];
+                }
+            }
+            return returnString;
+        }
+        public string ShowArrays()
+        {
+            string returnString = "\tIdx - Key => Value";
+            returnString += Environment.NewLine + "---------------------------------------------";
+            for (int i = 0; i < theValues.Length; i++)
+            {
+                returnString += Environment.NewLine + "[" + i + "] - " + theKeys[i] + " => " + theValues[i];
+            }
+            return returnString;
+        }
+
+    }
+
+
+}
--- cis237hashtable/Program.cs
+++ cis237hashtable/Program.cs
@@ -10,6 +10,120 @@ namespace cis237hashtable
     {
         static void Main(string[] args)
         {
+            //Instantiating a Generic Hash Class from this library
+            GenericHashMap<int, string> valleyNumToName = new GenericHashMap<int, string>();
+            //This is the built in C# Hash Class
+            Dictionary<int, string> valleyNumToNameDictionary = new Dictionary<int, string>();
+            //Setting the screen size
+            Console.SetBufferSize(80, 1200);
+
+            Console.WriteLine("Adding some entries to the HashMap");
+            Console.WriteLine("EX: valleyNumToName.Put(45678, \"Jean-Luc Picard\")");
+
+            valleyNumToName.Put(12345, "James Kirk");
+            valleyNumToName.Put(23456, "Benjamin Sisko");
+            valleyNumToName.Put(34567, "Kathryn Janeway");
+            valleyNumToName.Put(45678, "Jean-Luc Picard");
+            valleyNumToName.Put(56789, "Jonathan Archer");
+
+            Console.WriteLine();
+            Console.WriteLine("**************************************************");
+            Console.WriteLine("valleyNumToName.Get(45678)");
+            Console.WriteLine(valleyNumToName.Get(45678));
+            Console.WriteLine("**************************************************");
+            Console.WriteLine("Press Enter to Continue...");
+            Console.ReadLine();
+            Console.WriteLine();
+            
+            //Print the Hash table and array
+            Console.WriteLine("The Created Hash Table");
+            Console.WriteLine(valleyNumToName.ToString());
+            Console.WriteLine();
+            Console.WriteLine("The full array as it got filled out");
+            Console.WriteLine(valleyNumToName.ShowArrays());
+            Console.WriteLine("Press Enter to Continue...");
+            Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("**************************************************");
+            Console.WriteLine("Add some entries using the C# Dictionary");
+            Console.WriteLine("Adding some entries to the HashMap");
+            Console.WriteLine("EX: valleyNumToNameDictionary.Add(45678, \"Jean-Luc Picard\")");
+
+            valleyNumToNameDictionary.Add(12345, "James Kirk");
+            valleyNumToNameDictionary.Add(23456, "Benjamin Sisko");
+            valleyNumToNameDictionary.Add(34567, "Kathryn Janeway");
+            valleyNumToNameDictionary.Add(45678, "Jean-Luc Picard");
+            valleyNumToNameDictionary.Add(56789, "Jonathan Archer");
+
+            Console.WriteLine();
+            Console.WriteLine("**************************************************");
+            Console.WriteLine("valleyNumToNameDictionary.TryGetValue(45678)");
+            string theValueIWant;
+            valleyNumToNameDictionary.TryGetValue(45678, out theValueIWant);
+            Console.WriteLine(theValueIWant);
+            Console.WriteLine("**************************************************");
+            Console.WriteLine("Press Enter to Continue...");
+            Console.ReadLine();
+            Console.WriteLine();
+
+            //Print the Hash table and array
+            Console.WriteLine("The Created Hash Table");
+            var theArray = valleyNumToNameDictionary.ToArray();
+            for (int i = 0; i < theArray.Length; i++)
+            {
+                Console.WriteLine("[" + i + "] - " + " => " + theArray[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to Continue...");
+            Console.ReadLine();
+            Console.WriteLine();
+            
+            Console.WriteLine("**************************************************");
+            Console.WriteLine("What if we add 2 entries to the hash table");
+            Console.WriteLine("that map to the same location?");
+            Console.WriteLine("valleyNumToName.Put(26189, \"First Entry\")");
+            valleyNumToName.Put(26189, "First Entry");
+            Console.WriteLine("valleyNumToName.Put(26092, \"Second Entry\")");
+            valleyNumToName.Put(26092, "Second Entry");
+            Console.WriteLine();
+            Console.WriteLine("Let's try to get out the first entry");
+            Console.WriteLine("It should not exist since it was overwritten");
+            Console.WriteLine("by the second entry.  Therefore retrieving");
+            Console.WriteLine("the first will actually return the second.");
+            Console.WriteLine();
+            Console.WriteLine("valleyNumToName.Get(26189)");
+            Console.WriteLine(valleyNumToName.Get(26189));
+            Console.WriteLine("**************************************************");
+            Console.WriteLine("Press Enter to Continue...");
+            Console.ReadLine();
+            Console.WriteLine();
+
+            Console.WriteLine("**************************************************");
+            Console.WriteLine("What if we add 2 entries to the C# Dictionary");
+            Console.WriteLine("that map to the same location?");
+            Console.WriteLine("valleyNumToNameDictionary.Add(26189, \"First Entry\")");
+            valleyNumToNameDictionary.Add(26189, "First Entry");
+            Console.WriteLine("valleyNumToNameDictionary.Add(26092, \"Second Entry\")");
+            valleyNumToNameDictionary.Add(26092, "Second Entry");
+            Console.WriteLine();
+            Console.WriteLine("Let's try to get out the first entry");
+            Console.WriteLine("It should not exist since it was overwritten");
+            Console.WriteLine("by the second entry.  Therefore retrieving");
+            Console.WriteLine("the first will actually return the second.");
+            Console.WriteLine();
+            theArray = valleyNumToNameDictionary.ToArray();
+            for (int i = 0; i < theArray.Length; i++)
+            {
+                Console.WriteLine("[" + i + "] - " + " => " + theArray[i]);
+            }
+            Console.WriteLine("valleyNumToNameDictionary.TryGetValue(26189)");
+            valleyNumToNameDictionary.TryGetValue(26189, out theValueIWant);
+            Console.WriteLine(theValueIWant);
+            Console.WriteLine("**************************************************");
+            Console.WriteLine("Press Enter to Continue...");
+            Console.ReadLine();
+            Console.WriteLine();
         }
     }
 }
